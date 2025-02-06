78f9c664a2045b90f22ec61ffbca16b826659115
Harold Arguello <hjarguellor@gmail.com>
2025-02-06 15:35:10 -0600

    agregando componentes

---
+++ src/app/components/rols/rols.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rols',
+  imports: [],
+  templateUrl: './rols.component.html',
+  styleUrl: './rols.component.scss'
+})
+export class RolsComponent {
+
+}
