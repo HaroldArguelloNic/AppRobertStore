edb808850ede8a856be66810ade0c6b273af3bc7
Maximo <guzmanmax1998@gmail.com>
2025-02-06 13:02:50 -0600

    Base Listado card Productos.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {StoreComponent} from './Components/store/store.component';
+import {CartItemComponent} from './Components/cart-item/cart-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:StoreComponent,pathMatch:"full"},
+  {path:'store',component:StoreComponent,pathMatch:"full"},
+  {path:'carrito',component:CartItemComponent,pathMatch:"full"},
+  { path: "**", redirectTo: "store", pathMatch: "full" }
+];
