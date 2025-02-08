dd1733e969d738fd77f903ab8c93f55e1ba6213d
Maximo <guzmanmax1998@gmail.com>
2025-02-08 11:43:20 -0600

    Banner Base y Paginacion Base

--- src/app/Components/store/store.component.ts
+++ src/app/Components/store/store.component.ts
@@ -1,21 +1,24 @@
 import {Component, NgModule, OnInit} from '@angular/core';
 import { Product } from '../../core/Model/Product';
 import {CommonModule} from '@angular/common';
-import {ProductService} from '../../core/Services/product.service'; 
+import {ProductService} from '../../core/Services/product.service';
 import { HeaderComponent } from '../header/header.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 
 
 @Component({
   selector: 'app-store',
-  imports: [CommonModule, HeaderComponent, CarouselComponent ],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss']
 })
 export class StoreComponent implements OnInit {
-  listStartProduct: Product[] = [];  //
+  listStartProduct: Product[] = [];
+  paginatedProducts: Product[] = [];
+  currentPage:number = 1;
+  itemsForPage:number=10;
 
-  
+
 public slides = [
   { src: "/img/baner1.jpg" },
   { src: "/img/baner2.jpg" },
@@ -33,6 +36,7 @@ public slides = [
 
   ngOnInit() {
     this.loadProducts();
+
   }
 
   loadProducts() {
@@ -40,6 +44,7 @@ public slides = [
       next: (data) => {
         if (data.status) {
           this.listStartProduct = data.value;
+          this.updatePageProduct();
         } else {
           console.log('No se encontraron productos');
         }
@@ -48,6 +53,28 @@ public slides = [
         console.log('Error al cargar los productos', e);
       }
     });
+  }
+
+  updatePageProduct() {
+    const startIndex = (this.currentPage - 1) * this.itemsForPage;
+    const endIndex = startIndex + this.itemsForPage;
+    this.paginatedProducts = this.listStartProduct.slice(startIndex, endIndex);
+  }
+
+  changePageProduct(page: number) {
+    if (page >= 1 && page <= this.getTotalPages()) {
+      this.currentPage = page;
+      this.updatePageProduct();
+    }
+  }
+
+  getTotalPages(): number {
+    return Math.max(1, Math.ceil(this.listStartProduct.length / this.itemsForPage)); // Mínimo 1 página
+  }
+
+  getPageNumbers(): number[] {
+    return Array.from({ length: this.getTotalPages() }, (_, i) => i + 1);
+
   }
 
 }
